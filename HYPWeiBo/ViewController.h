5b4d23d702968469aa5c2054b20e70ea8dcfa7d5
StYaphet <947670170@qq.com>
2016-02-27 22:47:57 +0800

    Initial Commit

---
+++ HYPWeiBo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYPWeiBo
+//
+//  Created by 郝一鹏 on 16/2/27.
+//  Copyright © 2016年 bupt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
